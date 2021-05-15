ae1d3e3fd0ae6ffb0ac22ccaee4c3edd119dd97e
Nicolas Herschke <nicolas.herschke@gmx.de>
2021-05-15 16:06:53 +0200

    add declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NbsComponent } from './nbs/nbs.component';
+import { GreenRoofComponent } from './green-roof/green-roof.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    NbsComponent,
+    GreenRoofComponent
   ],
   imports: [
     BrowserModule,
