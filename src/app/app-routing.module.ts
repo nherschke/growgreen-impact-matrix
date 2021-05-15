f2447bcf337af6fc2eaf7ae1e7a4407726536a9b
Nicolas Herschke <nicolas.herschke@gmx.de>
2021-05-15 16:06:59 +0200

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GreenRoofComponent } from './green-roof/green-roof.component';
+import { HomeComponent } from './home/home.component';
+import { NbsComponent } from './nbs/nbs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'nbs', component: NbsComponent },
+  { path: 'green-roof', component: GreenRoofComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
