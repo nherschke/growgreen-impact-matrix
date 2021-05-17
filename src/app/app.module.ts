44084f766eff9b7952551404dc0978f082fda272
Nicolas Herschke <nicolas.herschke@gmx.de>
2021-05-17 11:06:11 +0200

    add about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NbsComponent } from './nbs/nbs.component';
 import { GreenRoofComponent } from './green-roof/green-roof.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { GreenRoofComponent } from './green-roof/green-roof.component';
     HomeComponent,
     NavbarComponent,
     NbsComponent,
-    GreenRoofComponent
+    GreenRoofComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
