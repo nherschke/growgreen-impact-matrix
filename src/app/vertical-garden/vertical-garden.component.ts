cdc65b153fa092d96e7ceed75894c06f0f9ac247
Nicolas Herschke <nicolas.herschke@gmx.de>
2021-05-25 15:34:30 +0200

    update reactive form

--- src/app/vertical-garden/vertical-garden.component.ts
+++ src/app/vertical-garden/vertical-garden.component.ts
@@ -1,26 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-vertical-garden',
   templateUrl: './vertical-garden.component.html',
-  styleUrls: ['./vertical-garden.component.css']
+  styleUrls: ['./vertical-garden.component.css'],
 })
 export class VerticalGardenComponent implements OnInit {
   inputForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.inputForm = this.formBuilder.group({
       precipitation: '',
       lifespan: '',
+      roofsize: [{ value: '', disabled: false }],
+      investment: [{ value: '', disabled: true }],
+      toggle: '1',
     });
 
-    this.inputForm.valueChanges.subscribe(console.log);
+    this.inputForm.get('toggle').valueChanges.subscribe((value) => {
+      if (value === '1') {
+        this.inputForm.get('investment').reset();
+        this.inputForm.get('investment').disable();
+        this.inputForm.get('roofsize').enable();
+      } else {
+        this.inputForm.get('roofsize').reset();
+        this.inputForm.get('roofsize').disable();
+        this.inputForm.get('investment').enable();
+      }
+    });
   }
 
   onSubmit(): void {
     console.log(this.inputForm);
+  }
+
+  checkToggle(): string {
+    let toggle = this.inputForm.get('toggle') as FormControl;
+    return toggle.value;
   }
 }
