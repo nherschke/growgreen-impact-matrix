a7d399ac6cf59f50ba4af19b21d4c7deb413ba75
Nicolas Herschke <nicolas.herschke@gmx.de>
2021-05-26 11:28:05 +0200

    first layout for output

--- src/app/output/output.component.ts
+++ src/app/output/output.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-output',
   templateUrl: './output.component.html',
-  styleUrls: ['./output.component.css']
+  styleUrls: ['./output.component.css'],
 })
 export class OutputComponent implements OnInit {
+  precipitation: number = 330;
+  lifespan: number = 22;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  active = 1;
 }
