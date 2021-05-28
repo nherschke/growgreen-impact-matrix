c850afe559565b844c85aef7b96f410a9018b545
Nicolas Herschke <nicolas.herschke@gmx.de>
2021-05-28 16:12:18 +0200

    add functions for each calculation method

--- src/app/calculation.service.ts
+++ src/app/calculation.service.ts
@@ -4,17 +4,30 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CalculationService {
+  precipitation: number;
+  lifespan: number;
+  wallSize: number;
+  investment: number;
+
   constructor() {}
 
   calculateWithWallSize(
     precipitation: number,
     lifespan: number,
     wallSize: number
-  ) {}
+  ) {
+    this.precipitation = precipitation;
+    this.lifespan = lifespan;
+    this.wallSize = wallSize;
+  }
 
   calculateWithInvestment(
     precipitation: number,
     lifespan: number,
     investment: number
-  ) {}
+  ) {
+    this.precipitation = precipitation;
+    this.lifespan = lifespan;
+    this.investment = investment;
+  }
 }
