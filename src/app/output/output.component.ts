06ce045a8314b3a10d818ef7e1322daa1c13bef8
Nicolas Herschke <nicolas.herschke@gmx.de>
2021-05-28 13:21:30 +0200

    check for method used

--- src/app/output/output.component.ts
+++ src/app/output/output.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AppData } from '../app.data';
 
 @Component({
   selector: 'app-output',
@@ -6,12 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./output.component.css'],
 })
 export class OutputComponent implements OnInit {
+  method: string;
   precipitation: number = 330;
   lifespan: number = 22;
+  wallSize: number = 50;
+  investment: number = 4000;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute, private data: AppData) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.method = params['method'];
+    });
+  }
 
   active = 1;
 }
