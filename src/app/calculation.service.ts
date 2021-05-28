51ed0c04bb39e1b50bc44045a7084dc94089ca98
Nicolas Herschke <nicolas.herschke@gmx.de>
2021-05-28 17:41:53 +0200

    complete environmental tab

--- src/app/calculation.service.ts
+++ src/app/calculation.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AppData } from './app.data';
 
 @Injectable({
   providedIn: 'root',
@@ -9,25 +10,35 @@ export class CalculationService {
   wallSize: number;
   investment: number;
 
-  constructor() {}
+  co2min: number;
+  co2max: number;
 
-  calculateWithWallSize(
+  retentionMin: number;
+  retentionMax: number;
+
+  constructor(private data: AppData) {}
+
+  setInput(
     precipitation: number,
     lifespan: number,
-    wallSize: number
-  ) {
-    this.precipitation = precipitation;
-    this.lifespan = lifespan;
-    this.wallSize = wallSize;
-  }
-
-  calculateWithInvestment(
-    precipitation: number,
-    lifespan: number,
+    wallSize: number,
     investment: number
   ) {
     this.precipitation = precipitation;
     this.lifespan = lifespan;
+    this.wallSize = wallSize;
     this.investment = investment;
   }
+
+  calculateWithWallSize() {
+    this.co2min =
+      this.wallSize * this.lifespan * this.data.CO2_SEQUESTRATION_MIN;
+    this.co2max =
+      this.wallSize * this.lifespan * this.data.CO2_SEQUESTRATION_MAX;
+
+    this.retentionMin = this.data.WATER_RETENTION_MIN * this.precipitation;
+    this.retentionMax = this.data.WATER_RETENTION_MAX * this.precipitation;
+  }
+
+  calculateWithInvestment() {}
 }
