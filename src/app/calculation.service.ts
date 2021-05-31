6c2eea79e9898cf643db9d62eea48cbf02dcfd1c
Nicolas Herschke <nicolas.herschke@gmx.de>
2021-05-31 17:35:36 +0200

    add first part of economic tab

--- src/app/calculation.service.ts
+++ src/app/calculation.service.ts
@@ -6,26 +6,41 @@ import { AppData } from './app.data';
   providedIn: 'root',
 })
 export class CalculationService {
+  // General
   precipitation: number;
   lifespan: number;
   wallSize: number;
   investment: number;
 
+  // Input Investment
   wallSizeMin: number;
   wallSizeMax: number;
 
+  // Environmental Tab
   co2min: number;
   co2max: number;
-
   retentionMin: number;
   retentionMax: number;
 
+  // Economic Tab
   investmentMin: number;
   investmentMax: number;
   maintenance: number;
   disposal: number;
   totalMin: number;
   totalMax: number;
+
+  airQualityCB: number;
+  smReductionMin: number;
+  smReductionMax: number;
+  energyReductionMin: number;
+  energyReductionMax: number;
+
+  wallLongevity: number;
+  propertyValue: number;
+
+  totalCostBenefitsMin: number;
+  totalCostBenefitsMax: number;
 
   constructor(private data: AppData) {}
 
@@ -55,6 +70,37 @@ export class CalculationService {
     this.disposal = this.wallSize * this.data.DISPOSAL;
     this.totalMin = this.investmentMin + this.maintenance + this.disposal;
     this.totalMax = this.investmentMax + this.maintenance + this.disposal;
+
+    this.airQualityCB =
+      this.wallSize * this.lifespan * this.data.AIR_QUALITY_MONETIZATION;
+
+    this.smReductionMin =
+      this.wallSize * this.lifespan * this.data.RUNOFF_COST_REDUCTION_MIN;
+    this.smReductionMax =
+      this.wallSize * this.lifespan * this.data.RUNOFF_COST_REDUCTION_MAX;
+
+    this.energyReductionMin =
+      this.wallSize * this.lifespan * this.data.ENERGY_COST_REDUCTION_MIN;
+    this.energyReductionMax =
+      this.wallSize * this.lifespan * this.data.ENERGY_COST_REDUCTION_MAX;
+
+    this.wallLongevity =
+      this.wallSize * this.lifespan * this.data.MEMBRANE_LONGEVITY_COST;
+    this.propertyValue =
+      this.wallSize * this.lifespan * this.data.PROPERTY_VALUE;
+
+    this.totalCostBenefitsMin =
+      this.airQualityCB +
+      this.smReductionMin +
+      this.energyReductionMin +
+      this.wallLongevity +
+      this.propertyValue;
+    this.totalCostBenefitsMax =
+      this.airQualityCB +
+      this.smReductionMax +
+      this.energyReductionMax +
+      this.wallLongevity +
+      this.propertyValue;
   }
 
   calculateWithInvestment() {
