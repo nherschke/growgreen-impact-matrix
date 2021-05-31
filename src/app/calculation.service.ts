1f542c136fb568f66db9c18ae9a6f443d4eefce0
Nicolas Herschke <nicolas.herschke@gmx.de>
2021-05-31 16:44:05 +0200

    add real costs calculation for input size

--- src/app/calculation.service.ts
+++ src/app/calculation.service.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { AppData } from './app.data';
 
@@ -10,11 +11,21 @@ export class CalculationService {
   wallSize: number;
   investment: number;
 
+  wallSizeMin: number;
+  wallSizeMax: number;
+
   co2min: number;
   co2max: number;
 
   retentionMin: number;
   retentionMax: number;
+
+  investmentMin: number;
+  investmentMax: number;
+  maintenance: number;
+  disposal: number;
+  totalMin: number;
+  totalMax: number;
 
   constructor(private data: AppData) {}
 
@@ -36,9 +47,34 @@ export class CalculationService {
     this.co2max =
       this.wallSize * this.lifespan * this.data.CO2_SEQUESTRATION_MAX;
 
+    this.calculateRetention();
+
+    this.investmentMin = this.wallSize * this.data.INVESTMENT_MIN;
+    this.investmentMax = this.wallSize * this.data.INVESTMENT_MAX;
+    this.maintenance = this.wallSize * this.data.MAINTENANCE;
+    this.disposal = this.wallSize * this.data.DISPOSAL;
+    this.totalMin = this.investmentMin + this.maintenance + this.disposal;
+    this.totalMax = this.investmentMax + this.maintenance + this.disposal;
+  }
+
+  calculateWithInvestment() {
+    this.wallSizeMin = this.investment / this.data.INVESTMENT_MAX;
+    this.wallSizeMax = this.investment / this.data.INVESTMENT_MIN;
+
+    this.co2min =
+      ((this.wallSizeMin + this.wallSizeMax) / 2) *
+      this.lifespan *
+      this.data.CO2_SEQUESTRATION_MIN;
+    this.co2max =
+      ((this.wallSizeMin + this.wallSizeMax) / 2) *
+      this.lifespan *
+      this.data.CO2_SEQUESTRATION_MAX;
+
+    this.calculateRetention();
+  }
+
+  calculateRetention() {
     this.retentionMin = this.data.WATER_RETENTION_MIN * this.precipitation;
     this.retentionMax = this.data.WATER_RETENTION_MAX * this.precipitation;
   }
-
-  calculateWithInvestment() {}
 }
